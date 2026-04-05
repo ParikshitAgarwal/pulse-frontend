b1f6d9a880594d6365bc6cf3954fe65051ca1ae8
Parikshit Agarwal <parikshitagarwal134@gmail.com>
2026-04-05 13:03:06 +0530

    updated vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,8 @@
-import { defineConfig } from 'vite'
+import {
+  defineConfig
+} from 'vite'
 import react from '@vitejs/plugin-react'
-import { config } from './src/config'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': config.API_URL,
-      '/socket.io': {
-        target: config.SOCKET_URL,
-        ws: true
-      }
-    }
-  }
 })
